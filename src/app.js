f0af8cf24722000843d8f2d13701157a5031bb4a
Arpit Tripathi <arpittripathi254@gmail.com>
2026-06-29 15:03:46 +0530

    Remove temporary test db

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 import clientRoutes from "./routes/clientRoutes.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
 import billingRoutes from "./routes/billingRoutes.js";
-import pool from "./config/db.js";
 
 dotenv.config();
 
@@ -23,22 +22,6 @@ app.use("/api/billing", billingRoutes);
 // Test route
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
-});
-
-app.get("/test-db", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json({
-      success: true,
-      time: result.rows[0],
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      success: false,
-      error: err.message,
-    });
-  }
 });
 
 // Server
