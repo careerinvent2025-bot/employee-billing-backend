c35aaf0c5faa7697b5e981696075f792bfc8e525
Arpit Tripathi <arpittripathi254@gmail.com>
2026-06-29 11:53:08 +0530

    Added database connection test

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import clientRoutes from "./routes/clientRoutes.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
 import billingRoutes from "./routes/billingRoutes.js";
+import pool from "./config/db.js";
 
 dotenv.config();
 
@@ -22,6 +23,22 @@ app.use("/api/billing", billingRoutes);
 // Test route
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      success: true,
+      time: result.rows[0],
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
 });
 
 // Server
