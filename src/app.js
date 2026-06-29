c7b44c0fe24b7ed1e2cbedb2b1409e78946a23f1
Arpit Tripathi <arpittripathi254@gmail.com>
2026-06-29 16:56:40 +0530

    add db check path

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import clientRoutes from "./routes/clientRoutes.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
 import billingRoutes from "./routes/billingRoutes.js";
+import pool from "./config/db.js";
 
 dotenv.config();
 
@@ -22,6 +23,16 @@ app.use("/api/billing", billingRoutes);
 // Test route
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
+});
+
+app.get("/db-check", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT current_database(), current_user;");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err.message);
+  }
 });
 
 // Server
